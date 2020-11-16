172e4580199f0059d8df2d4608133eaee16f09b0
ReditaJ <redita.j@gmail.com>
2020-11-16 21:35:59 +0200

    paskaita

--- 3_JS/scripts /script.js
+++ 3_JS/scripts /script.js
@@ -59,3 +59,19 @@ button1.addEventListener("click", function() {
 document.addEventListener("keyup", function(event) {
     console.log(event.key);
 });
+
+
+var button2 = document.querySelector("#change-img");
+var i=0;
+button2.addEventListener("click", function() {
+    var image = document.querySelector("div.gallery img");
+    image.setAttribute("src", "http://picsum.photos/seed/" + i++ + "/150");
+});
+
+var images = document.querySelectorAll("dic.gallery img");
+for (var j = 0; j < images.length; j++) {
+    images[j].addEventListener("click", function(event){
+        console.log(event);
+
+    });
+}
